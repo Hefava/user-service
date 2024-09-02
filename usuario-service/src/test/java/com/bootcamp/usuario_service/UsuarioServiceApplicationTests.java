4d82560505a224d9c7b86b6b4f636ba9491b6162
Héctor Fabio Vanegas Guarín <114118173+Hefava@users.noreply.github.com>
2024-09-01 20:24:58 -0500

    created user microservice

---
+++ usuario-service/src/test/java/com/bootcamp/usuario_service/UsuarioServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.usuario_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuarioServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
